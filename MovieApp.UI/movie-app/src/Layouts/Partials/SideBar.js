119957fcdc053e676dcdea05b8543d4d310bcd0d
korayyalcin1903 <korayyalcin1903@gmail.com>
2025-02-16 21:37:12 +0300

    Simplify sidebar by removing multiple navigation items
    The code changes in `SideBar.js` involve the removal of several navigation items and their associated HTML structure. Specifically, the following items were removed:
    - A navigation item for "Sports" with an icon and link to `sports.html`.
    - A horizontal rule and a sidebar heading labeled "Extra".
    - A collapsible navigation item for "Pages" with links to various pages such as "Movies", "Events", "People", and "Sports", along with their respective detail pages.
    - A navigation item for "Offers" with an icon and link to `offers.html`.
    - A collapsible navigation item for "Extra Pages" with links to login-related pages and other pages like "404 Page" and "Blank Page".
    
    These changes simplify the sidebar by removing several navigation items and collapsible sections, potentially to streamline the user interface or to remove outdated or unnecessary links.

--- MovieApp.UI/movie-app/src/Layouts/Partials/SideBar.js
+++ MovieApp.UI/movie-app/src/Layouts/Partials/SideBar.js
@@ -27,60 +27,8 @@ return (
                 <i className="fas fa-fw fa-users"></i>
                 <span>Cast</span></NavLink>
         </li>
-        <li className="nav-item">
-            <Link className="nav-link" href="sports.html">
-                <i className="fas fa-fw fa-futbol"></i>
-                <span>Sports</span></Link>
-        </li>
-
         <hr className="sidebar-divider" />
 
-        <div className="sidebar-heading">Extra</div>
-
-        <li className="nav-item">
-            <Link className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-                aria-controls="collapseTwo">
-                <i className="fas fa-fw fa-pager"></i>
-                <span>Pages</span>
-            </Link>
-            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                <div className="bg-white py-2 collapse-inner rounded">
-                    <h6 className="collapse-header">All Pages</h6>
-                    <Link className="collapse-item" href="movies.html">Movies</Link>
-                    <Link className="collapse-item" href="movies-detail.html">Movie Detail</Link>
-                    <Link className="collapse-item" href="events.html">Events</Link>
-                    <Link className="collapse-item" href="events-detail.html">Event Detail</Link>
-                    <Link className="collapse-item" href="people.html">People</Link>
-                    <Link className="collapse-item" href="people-detail.html">People Detail</Link>
-                    <Link className="collapse-item" href="sports.html">Sports</Link>
-                    <Link className="collapse-item" href="sports-detail.html">Sport Detail</Link>
-                </div>
-            </div>
-        </li>
-        <li className="nav-item">
-            <Link className="nav-link" href="offers.html">
-                <i className="fas fa-fw fa-fire"></i>
-                <span>Offers</span></Link>
-        </li>
-        <li className="nav-item">
-            <Link className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-                aria-controls="collapsePages">
-                <i className="fas fa-fw fa-clone"></i>
-                <span>Extra Pages</span>
-            </Link>
-            <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                <div className="bg-white py-2 collapse-inner rounded">
-                    <h6 className="collapse-header">Login Screens:</h6>
-                    <Link className="collapse-item" to="/login">Login</Link>
-                    <Link className="collapse-item" to="/register">Register</Link>
-                    <Link className="collapse-item" href="forgot-password.html">Forgot Password</Link>
-                    <div className="collapse-divider"></div>
-                    <h6 className="collapse-header">Other Pages:</h6>
-                    <Link className="collapse-item" href="404.html">404 Page</Link>
-                    <Link className="collapse-item" href="blank.html">Blank Page</Link>
-                </div>
-            </div>
-        </li>
     </ul>
 )
 }
